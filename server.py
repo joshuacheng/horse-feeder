876a853f1b0ae34e67bc581222eaa18d742d73f5
ey1031 <eyudeveloper@gmail.com>
2019-02-01 19:26:03 -0800

    heroku deployment

--- server.py
+++ server.py
@@ -7,6 +7,17 @@ import json
 # from image_line import execute_zhang_suen
 import base64
 
+# firebase shit
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import firestore
+# cred = credentials.ApplicationDefault()
+# firebase_admin.initialize_app(cred, {
+#   'projectId': project_id,
+# })
+
+db = firestore.client()
+
 app = Flask(__name__)
 
 @app.route('/')
