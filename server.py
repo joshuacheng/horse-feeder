4b8def949c5db7ddb46e1e0d98d39441df2d12f4
ey1031 <eyudeveloper@gmail.com>
2019-02-01 19:39:01 -0800

    gitignore and firebase

--- server.py
+++ server.py
@@ -7,14 +7,14 @@ import json
 # from image_line import execute_zhang_suen
 import base64
 
-# firebase shit
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore
-# cred = credentials.ApplicationDefault()
-# firebase_admin.initialize_app(cred, {
-#   'projectId': project_id,
-# })
+firebase shit
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': project_id,
+})
 
 db = firestore.client()
 
